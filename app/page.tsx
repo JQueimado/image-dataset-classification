4727c5145cd812ce6f7006ffef10ca285d499bc0
JQueimado <jpqueimado@gmail.com>
2024-02-23 01:35:24 +0000

    add image list

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,36 @@
 import UploadForm from "./UploadForm";
+import fs from "fs";
+import path from "path";
+import Image from "next/image";
 
-// export const getServerSideProps = (async () => {
-//   const images: Images = { names: [] };
-//   try {
-//     images.names = fs.readdirSync(
-//       path.join(process.cwd(), "/public/imgs")
-//     ) as string[];
-//     console.log(images.names);
-//     return { props: { images } };
-//   } catch (e) {
-//     return { props: { images } };
-//   }
-// }) satisfies GetServerSideProps<{ images: Images }>;
+interface Images {
+  names: string[];
+}
 
 export default function Home() {
+  const images: Images = { names: [] };
+  try {
+    images.names = fs.readdirSync(
+      path.join(process.cwd(), "/public/imgs")
+    ) as string[];
+    console.log(images.names);
+  } catch (e) {}
+
   return (
     <main className="relative w-full">
       <div className="absoulute top-0 left-0 right-0 w-1/6 min-h-screen bg-base-300">
         <h1 className="text-center">Images</h1>
         <UploadForm />
         <div className="overflow-x">
-          {/* {images?.names.map((dir) => (
+          {images?.names.map((dir, i) => (
             <Image
               src={`/imgs/${dir}`}
               width={500}
               height={500}
               alt={"image"}
+              key={i}
             />
-          ))} */}
+          ))}
         </div>
       </div>
     </main>
